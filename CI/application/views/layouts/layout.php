f4a87db8fa75486879261acf3a965b54910cc634
leejaekwon <nowkwon97@gmail.com>
2023-07-28 15:47:28 +0900

    SET css파일 구조화 수정

--- CI/application/views/layouts/layout.php
+++ CI/application/views/layouts/layout.php
@@ -3,7 +3,7 @@
 
 <head>
   <title>title</title>
-  <link rel="stylesheet" href="<?php echo base_url('public/css/styles.css'); ?>">
+  <link rel="stylesheet" href="<?php echo base_url('public/views/layouts/css/styles.css'); ?>">
 </head>
 
 <body>
